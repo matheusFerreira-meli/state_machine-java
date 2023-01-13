6b2b079a79ff1d3f2ac8a8b24cc44cf1b96e1df4
matheusFerreira-meli <m.ferreira.araujo2016@gmail.com>
2023-01-13 18:42:43 -0300

    feat: criando o projeto

---
+++ src/main/java/com/example/statemachinejava/StateMachineJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.statemachinejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateMachineJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateMachineJavaApplication.class, args);
+    }
+
+}
